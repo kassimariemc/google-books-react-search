ee50d4dc9c4320c3eb2ecbe51bf03bab5cb97ba4
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-11-15 13:57:22 -0500

    initial commit with starter files

---
+++ client/build/precache-manifest.52d85e093a337bf649c65780910ecd6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbc761e6410293e090e13cbef3827b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c37b8b991f0257a1ce4",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "5309e449abb858676493",
+    "url": "/static/js/2.7f4868fe.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.7f4868fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c37b8b991f0257a1ce4",
+    "url": "/static/js/main.922be00a.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
